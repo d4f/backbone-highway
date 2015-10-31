8abea09bc843ab1d66a159cbf35c5bcffd6d5377
RasCarlito <rascarlito@gmail.com>
2015-10-31 10:42:05 +0100

    Added some tests

--- test/spec/backbone.highway.spec.js
+++ test/spec/backbone.highway.spec.js
@@ -7,9 +7,21 @@ define([
   // jshint -W030
 
   describe('Methods', function () {
-    it('should have a start method', function () {
+    it('should have a `start` method', function () {
       expect(
         _.isFunction(router.start)
+      ).to.be.ok;
+    });
+
+    it('should have a `map` method', function () {
+      expect(
+        _.isFunction(router.map)
+      ).to.be.ok;
+    });
+
+    it('should have a `route` method', function () {
+      expect(
+        _.isFunction(router.route)
       ).to.be.ok;
     });
   });
